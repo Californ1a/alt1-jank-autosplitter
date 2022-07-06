69dcea17faeaba4d8ceedcbc33dff4e451dfd9f6
Californ1a <rk_california@yahoo.com>
2022-07-06 01:46:18 +0000

    Deploying to gh-pages from @ Californ1a/alt1-jank-autosplitter@e766e7338f65355fd00ff3ced2d6ab0870fbded9 🚀

--- index.bundle.js
+++ index.bundle.js
@@ -4285,12 +4285,23 @@ let timerAnim;
 let splits = [];
 let lastTime = (new Date()).getTime();
 let startTime = 0;
+let file;
+function onError(e) {
+    console.error(e);
+}
 function clear() {
     cancelAnimationFrame(timerAnim);
     startTime = 0;
     splitsEle.innerHTML = "";
     timerEle.innerHTML = "0.<span class=\"miliseconds\">00</span>";
     splits = [];
+    if (file) {
+        file.createWriter((fileWriter) => {
+            fileWriter.seek(fileWriter.length);
+            const blob = new Blob(["RESET\r\n"], { type: "text/plain" });
+            fileWriter.write(blob);
+        }, onError);
+    }
 }
 function formatTime(value) {
     const seconds = (value / 1000) % 60;
@@ -4321,9 +4332,9 @@ function timer() {
     }
     timerAnim = requestAnimationFrame(timer);
 }
-function split(actions, fileEntry, onError) {
-    if (fileEntry && onError) {
-        fileEntry.createWriter((fileWriter) => {
+function split(actions) {
+    if (file) {
+        file.createWriter((fileWriter) => {
             fileWriter.seek(fileWriter.length);
             const blob = new Blob([`CAL-SPLIT-${actions}\r\n`], { type: "text/plain" });
             fileWriter.write(blob);
@@ -4367,108 +4378,117 @@ function capture() {
         //timerEle.innerHTML = "0.<span class=\"miliseconds\">00</span>";
         return;
     }
-    function editFile(fileEntry) {
-        const appColor = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0);
-        let reader = new _alt1_chatbox__WEBPACK_IMPORTED_MODULE_1__["default"]();
-        reader.readargs = {
-            colors: [
-                _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 255, 255),
-                _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0), //green
-                //A1lib.mixColor(255, 165, 0), //Scavenging comps
-                //A1lib.mixColor(255, 0, 0), //Rare Mats
-                //A1lib.mixColor(67, 188, 188), //Ancient components
-            ],
-        };
+    const appColor = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0);
+    let reader = new _alt1_chatbox__WEBPACK_IMPORTED_MODULE_1__["default"]();
+    reader.readargs = {
+        colors: [
+            _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 255, 255),
+            _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0), //green
+            //A1lib.mixColor(255, 165, 0), //Scavenging comps
+            //A1lib.mixColor(255, 0, 0), //Rare Mats
+            //A1lib.mixColor(67, 188, 188), //Ancient components
+        ],
+    };
+    try {
+        reader.find(); //Find the chat box.
+        reader.read(); //Get the initial read, to not report on initial load.
+    }
+    catch (e) {
+        console.error(e);
+        timerEle.innerHTML = "<small>Failed to capture RS window</small>";
+        return;
+    }
+    function showSelectedChat(chat) {
+        //Attempt to show a temporary rectangle around the chatbox.  skip if overlay is not enabled.
         try {
-            reader.find(); //Find the chat box.
-            reader.read(); //Get the initial read, to not report on initial load.
+            alt1.overLayRect(appColor, chat.mainbox.rect.x, chat.mainbox.rect.y, chat.mainbox.rect.width, chat.mainbox.rect.height, 2000, 5);
         }
-        catch (e) {
-            console.error(e);
-            timerEle.innerHTML = "<small>Failed to capture RS window</small>";
-            return;
-        }
-        function showSelectedChat(chat) {
-            //Attempt to show a temporary rectangle around the chatbox.  skip if overlay is not enabled.
-            try {
-                alt1.overLayRect(appColor, chat.mainbox.rect.x, chat.mainbox.rect.y, chat.mainbox.rect.width, chat.mainbox.rect.height, 2000, 5);
+        catch (_a) { }
+    }
+    //Find all visible chatboxes on screen
+    // reader.find();
+    // reader.read();
+    let findChat = setInterval(() => {
+        if (reader.pos === null)
+            reader.find();
+        else {
+            clearInterval(findChat);
+            // reader.pos.boxes.map((box, i) => {
+            // 	$(".chat").append(`<option value=${i}>Chat ${i}</option>`);
+            // });
+            if (localStorage.ccChat) {
+                reader.pos.mainbox = reader.pos.boxes[localStorage.ccChat];
             }
-            catch (_a) { }
-        }
-        //Find all visible chatboxes on screen
-        // reader.find();
-        // reader.read();
-        let findChat = setInterval(() => {
-            if (reader.pos === null)
-                reader.find();
             else {
-                clearInterval(findChat);
-                // reader.pos.boxes.map((box, i) => {
-                // 	$(".chat").append(`<option value=${i}>Chat ${i}</option>`);
-                // });
-                if (localStorage.ccChat) {
-                    reader.pos.mainbox = reader.pos.boxes[localStorage.ccChat];
+                //If multiple boxes are found, this will select the first, which should be the top-most chat box on the screen.
+                reader.pos.mainbox = reader.pos.boxes[0];
+            }
+            showSelectedChat(reader.pos);
+            chatboxInterval = setInterval(() => {
+                readChatbox();
+            }, 500);
+            startBtn.innerHTML = "Stop";
+            startTime = (new Date()).getTime();
+            requestAnimationFrame(timer);
+        }
+    }, 1000);
+    let timestamps = new Set();
+    let actions = 1;
+    function readChatbox() {
+        const opts = reader.read() || [];
+        let chat = "";
+        for (let a in opts) {
+            chat += opts[a].text + " ";
+        }
+        //console.log(chat);
+        const clueComplete = chat.match(/\[\d{2}:\d{2}:\d{2}\] Your (reward was stored in Charos|Charos Clue Carrier has placed)/g);
+        if (clueComplete != null && clueComplete.length > -1) {
+            console.log(clueComplete);
+            const timestamp = clueComplete[0].match(/\d{2}:\d{2}:\d{2}/g);
+            if (timestamp != null && timestamp.length > -1) {
+                if (timestamps.has(timestamp[0])) {
+                    console.log("Duplicate timestamp");
                 }
                 else {
-                    //If multiple boxes are found, this will select the first, which should be the top-most chat box on the screen.
-                    reader.pos.mainbox = reader.pos.boxes[0];
-                }
-                showSelectedChat(reader.pos);
-                chatboxInterval = setInterval(() => {
-                    readChatbox();
-                }, 500);
-                startBtn.innerHTML = "Stop";
-                startTime = (new Date()).getTime();
-                requestAnimationFrame(timer);
-            }
-        }, 1000);
-        let timestamps = new Set();
-        let actions = 1;
-        function readChatbox() {
-            const opts = reader.read() || [];
-            let chat = "";
-            for (let a in opts) {
-                chat += opts[a].text + " ";
-            }
-            //console.log(chat);
-            const clueComplete = chat.match(/\[\d{2}:\d{2}:\d{2}\] Your (reward was stored in Charos|Charos Clue Carrier has placed)/g);
-            if (clueComplete != null && clueComplete.length > -1) {
-                console.log(clueComplete);
-                const timestamp = clueComplete[0].match(/\d{2}:\d{2}:\d{2}/g);
-                if (timestamp != null && timestamp.length > -1) {
-                    if (timestamps.has(timestamp[0])) {
-                        console.log("Duplicate timestamp");
-                    }
-                    else {
-                        timestamps.add(timestamp[0]);
-                        console.log("SPLIT!");
-                        split(actions, fileEntry, onError);
-                        actions++;
-                    }
+                    timestamps.add(timestamp[0]);
+                    console.log("SPLIT!");
+                    split(actions);
+                    actions++;
                 }
             }
         }
-        fileEntry.createWriter((fileWriter) => {
-            fileWriter.truncate(0);
-        }, onError);
     }
-    function onError(e) {
-        console.error(e);
-    }
-    function onInitFs(fs) {
-        fs.root.getFile("info.txt", { create: false }, editFile, (e) => {
-            console.log("File does not exist, creating...");
-            fs.root.getFile("info.txt", { create: true }, editFile, onError);
-        });
-    }
-    window.webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024, onInitFs, onError);
 }
+function truncate(fs) {
+    fs.createWriter((fileWriter) => {
+        fileWriter.truncate(0);
+    }, onError);
+}
+function setFile(fs) {
+    truncate(fs);
+    file = fs;
+}
+function onInitFs(fs) {
+    fs.root.getFile("info.txt", { create: false }, setFile, (e) => {
+        console.log("File does not exist, creating...");
+        fs.root.getFile("info.txt", { create: true }, setFile, onError);
+    });
+}
+window.webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024, onInitFs, onError);
 //check if we are running inside alt1 by checking if the alt1 global exists
 if (window.alt1) {
     //tell alt1 about the app
     //this makes alt1 show the add app button when running inside the embedded browser
     //also updates app settings if they are changed
     alt1.identifyAppUrl("./appconfig.json");
+    if (!alt1.permissionPixel) {
+        timerEle.innerHTML = "<small>Page is not installed as app or capture permission is not enabled</small>";
+    }
+}
+if (document.location.host !== "californ1a.github.io") {
+    const th = document.createElement("th");
+    th.innerHTML = "<button class=\"nisbutton test\">Test</button>";
+    document.querySelector(".menu tr").appendChild(th);
 }
 const testBtn = document.querySelector(".nisbutton.test");
 if (testBtn) {
