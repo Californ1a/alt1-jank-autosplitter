5363d67af186f85633d6af7c28d4203402948d54
Californ1a <rk_california@yahoo.com>
2022-07-07 02:10:12 +0000

    Deploying to gh-pages from @ Californ1a/alt1-jank-autosplitter@4e9a3b95c79abe949caacc2bd63da5658e13da25 🚀

--- index.bundle.js
+++ index.bundle.js
@@ -4139,6 +4139,21 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./settingsbutton2.png":
+/*!***************************************************************************************!*\
+  !*** ../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./settingsbutton2.png ***!
+  \***************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "settingsbutton2.png");
+
+/***/ }),
+
 /***/ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./style.css":
 /*!*****************************************************************************!*\
   !*** ../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./style.css ***!
@@ -4272,26 +4287,172 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__(/*! !file-loader?name=[name].[ext]!./index.html */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./index.html");
 __webpack_require__(/*! !file-loader?name=[name].[ext]!./style.css */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./style.css");
 __webpack_require__(/*! !file-loader?name=[name].[ext]!./appconfig.json */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./appconfig.json");
+__webpack_require__(/*! !file-loader?name=[name].[ext]!./settingsbutton2.png */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./settingsbutton2.png");
 // const output = document.querySelector(".main");
 const startBtn = document.querySelector(".nisbutton.start");
 startBtn.addEventListener("click", startTimer);
 const clearBtn = document.querySelector(".nisbutton.clear");
 clearBtn.addEventListener("click", clear);
-const timerEle = document.querySelector(".timer");
+const settingsBtn = document.querySelector(".nissmallimagebutton.settings");
+settingsBtn.addEventListener("click", openSettings);
+const modal = document.getElementById("settingsModal");
+const modalCloseBtn = document.getElementsByClassName("nisclosebutton")[0];
+const timerEle = document.querySelector(".timer .time");
 const splitsEle = document.querySelector(".splits");
 const scrollBox = document.querySelector(".second");
+const defaultButton = document.querySelector(".default");
+let errorEle = document.querySelector(".error");
+const regexTimestampStr = "\\[\\d{2}:\\d{2}:\\d{2}\\]";
+const regexStr = "Your (reward was stored in Charos|Charos Clue Carrier has placed)";
+let regex = new RegExp(`${regexTimestampStr} ${regexStr}`);
 let chatboxInterval;
 let timerAnim;
 let splits = [];
 let lastTime = (new Date()).getTime();
 let startTime = 0;
 let file;
+let actions = 1;
+let reader;
+function closeSettings() {
+    const settingsElements = document.querySelectorAll(".modal-content td:nth-child(even)");
+    for (const { children: [setting] } of settingsElements) {
+        // console.log(setting.id, setting.value);
+        if (setting.id === "regex") {
+            regex = new RegExp(`${regexTimestampStr} ${setting.value}`);
+        }
+        else if (setting.id === "chat") {
+            const chat = setting.value;
+            const ls = localStorage.getItem("chat");
+            if (chat !== null && chat !== "") {
+                const main = reader.pos.mainbox;
+                const box = JSON.parse(atob(chat));
+                if (main.rect.x !== box.rect.x || main.rect.y !== box.rect.y) { // not the same box
+                    reader.pos.mainbox = box;
+                }
+            }
+            else if (ls !== "" && ls !== null) {
+                reader.pos.mainbox = JSON.parse(atob(ls));
+            }
+            else {
+                //If multiple boxes are found, this will select the first, which should be the top-most chat box on the screen.
+                reader.pos.mainbox = reader.pos.boxes[0];
+            }
+            showSelectedChat(reader.pos.mainbox);
+        }
+        else if (setting.id === "color") {
+            const c = hexToRgb(setting.value);
+            reader.readargs.colors[reader.readargs.colors.length - 1] = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(c[0], c[1], c[2]);
+        }
+        if (setting.type === "checkbox") {
+            localStorage.setItem(setting.id, setting.checked);
+        }
+        else {
+            localStorage.setItem(setting.id, setting.value);
+        }
+    }
+    modal.style.display = "none";
+}
+defaultButton.addEventListener("click", () => {
+    localStorage.clear();
+    const settingsElements = document.querySelectorAll(".modal-content td:nth-child(even)");
+    for (const { children: [setting] } of settingsElements) {
+        if (setting.id === "regex") {
+            setting.value = regexStr;
+            regex = new RegExp(`${regexTimestampStr} ${setting.value}`);
+        }
+        else if (setting.id === "chat") {
+            setting.value = "";
+            reader.pos.mainbox = reader.pos.boxes[0];
+            showSelectedChat(reader.pos.mainbox);
+        }
+        else if (setting.id === "livesplit") {
+            setting.checked = true;
+            localStorage.setItem("livesplit", "true");
+        }
+        else if (setting.id === "timer-type") {
+            setting.value = "overall";
+            localStorage.setItem("timer-type", "overall");
+        }
+        else if (setting.id === "color") {
+            setting.value = "#00ff00";
+            localStorage.setItem("color", "#00ff00");
+        }
+    }
+});
+modalCloseBtn.addEventListener("click", () => {
+    closeSettings();
+});
+window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        closeSettings();
+    }
+});
+errorEle.addEventListener("mouseenter", () => {
+    alt1.setTooltip(errorEle.ariaLabel);
+});
+errorEle.addEventListener("mouseleave", () => {
+    alt1.clearTooltip();
+});
+document.querySelector("#chat").addEventListener("change", (e) => {
+    const value = e.target.value || localStorage.getItem("chat");
+    if (value) {
+        showSelectedChat(JSON.parse(atob(e.target.value)));
+    }
+    else {
+        showSelectedChat(reader.pos.mainbox);
+    }
+});
+function openSettings() {
+    modal.style.display = "flex";
+    const regexEle = document.querySelector("textarea#regex");
+    if (!regexEle.value) {
+        regexEle.value = regexStr;
+    }
+    const livesplitEle = document.querySelector("#livesplit");
+    const ls = localStorage.getItem("livesplit");
+    if (ls === null || ls === "") {
+        livesplitEle.checked = true;
+    }
+    else if (ls === "false") {
+        livesplitEle.checked = false;
+    }
+    else {
+        livesplitEle.checked = true;
+    }
+    const timerTypeEle = document.querySelector("#timer-type");
+    const tt = localStorage.getItem("timer-type");
+    if (tt === null || tt === "") {
+        timerTypeEle.value = "overall";
+    }
+    else {
+        timerTypeEle.value = tt;
+    }
+    const colorEle = document.querySelector("#color");
+    const c = localStorage.getItem("color");
+    if (c === null || c === "") {
+        colorEle.value = "#00ff00";
+    }
+    else {
+        colorEle.value = c;
+    }
+}
+function setError(message) {
+    if (!errorEle) {
+        errorEle = document.createElement("span");
+        errorEle.className = "error";
+        timerEle.append(errorEle);
+    }
+    errorEle.style.display = "block";
+    errorEle.ariaLabel = message;
+}
 function onError(e) {
     console.error(e);
 }
 function clear() {
+    startBtn.innerHTML = "Start";
     cancelAnimationFrame(timerAnim);
     startTime = 0;
+    actions = 1;
     splitsEle.innerHTML = "";
     timerEle.innerHTML = "0.<span class=\"miliseconds\">00</span>";
     splits = [];
@@ -4322,20 +4483,25 @@ function timer() {
         lastTime = currentTime;
         // numSeconds++;
         // timerEle.innerHTML = `${numSeconds}`;
-        showTime(currentTime - startTime);
+        if (localStorage.getItem("timer-type") === "single") {
+            const previous = splits[splits.length - 1] || startTime;
+            showTime(currentTime - previous);
+        }
+        else {
+            showTime(currentTime - startTime);
+        }
     }
     timerAnim = requestAnimationFrame(timer);
 }
-function split(actions) {
+function split() {
     writeLine(`SPLIT-${actions}`);
     const currentTime = (new Date()).getTime();
-    const previous = splits[splits.length - 1];
-    splits.push(currentTime - startTime);
-    const current = splits[splits.length - 1];
+    const previous = splits[splits.length - 1] || startTime;
+    splits.push(currentTime);
     const msDuration = currentTime - startTime;
     const [cluesHr, chrMs] = `${((actions / msDuration) * (60 * 60 * 1000)).toFixed(2)}`.split("."); // clues/hr using total time avg
     const time = formatTime(msDuration);
-    const segMsDur = current - previous;
+    const segMsDur = currentTime - previous;
     // const [cluesHr, chrMs] = `${((1/segMsDur) * (60 * 60 * 1000)).toFixed(2)}`.split("."); // clues/hr using segment time
     const segmentTime = (previous) ? formatTime(segMsDur) : time;
     splitsEle.innerHTML += `<tr>
@@ -4350,7 +4516,7 @@ function split(actions) {
     });
 }
 function writeLine(line) {
-    if (file) {
+    if (file && localStorage.getItem("livesplit") === "true") {
         file.createWriter((fileWriter) => {
             fileWriter.seek(fileWriter.length);
             const blob = new Blob([`${line}\r\n`], { type: "text/plain" });
@@ -4372,21 +4538,37 @@ function startTimer() {
     startTime = (new Date()).getTime();
     requestAnimationFrame(timer);
 }
+function showSelectedChat(chat) {
+    const appColor = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0);
+    //Attempt to show a temporary rectangle around the chatbox.  skip if overlay is not enabled.
+    try {
+        alt1.overLayRect(appColor, chat.rect.x, chat.rect.y, chat.rect.width, chat.rect.height, 2000, 5);
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
 function capture() {
     if (!window.alt1) {
-        timerEle.innerHTML = "<small>You need to run this page in alt1 to capture the screen</small>";
+        setError("You need to run this page in alt1 to capture the screen");
         return;
     }
     if (!alt1.permissionPixel) {
-        timerEle.innerHTML = "<small>Page is not installed as app or capture permission is not enabled</small>";
+        setError("Page is not installed as app or capture permission is not enabled");
         return;
     }
-    const appColor = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0);
-    let reader = new _alt1_chatbox__WEBPACK_IMPORTED_MODULE_1__["default"]();
+    reader = new _alt1_chatbox__WEBPACK_IMPORTED_MODULE_1__["default"]();
+    let c = localStorage.getItem("color");
+    if (c) {
+        c = hexToRgb(c);
+    }
+    else {
+        c = [0, 255, 0];
+    }
     reader.readargs = {
         colors: [
             _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 255, 255),
-            _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0), //green
+            _alt1_base__WEBPACK_IMPORTED_MODULE_0__.mixColor(c[0], c[1], c[2]), //green
             //A1lib.mixColor(255, 165, 0), //Scavenging comps
             //A1lib.mixColor(255, 0, 0), //Rare Mats
             //A1lib.mixColor(67, 188, 188), //Ancient components
@@ -4398,17 +4580,8 @@ function capture() {
     }
     catch (e) {
         console.error(e);
-        timerEle.innerHTML = "<small>Failed to capture RS window</small>";
+        setError("Failed to capture RS");
         return;
-    }
-    function showSelectedChat(chat) {
-        //Attempt to show a temporary rectangle around the chatbox.  skip if overlay is not enabled.
-        try {
-            alt1.overLayRect(appColor, chat.mainbox.rect.x, chat.mainbox.rect.y, chat.mainbox.rect.width, chat.mainbox.rect.height, 2000, 5);
-        }
-        catch (e) {
-            console.error(e);
-        }
     }
     //Find all visible chatboxes on screen
     // reader.find();
@@ -4418,25 +4591,27 @@ function capture() {
             reader.find();
         else {
             clearInterval(findChat);
-            // reader.pos.boxes.map((box, i) => {
-            // 	$(".chat").append(`<option value=${i}>Chat ${i}</option>`);
-            // });
-            // TODO: Add a dropdown to select which chat to read.
-            if (localStorage.ccChat) {
-                reader.pos.mainbox = reader.pos.boxes[localStorage.ccChat];
+            reader.pos.boxes.map((box, i) => {
+                const chat = document.querySelector("#chat");
+                const value = btoa(JSON.stringify(box));
+                chat.innerHTML += `<option value=${value}>Chat ${i}</option>`;
+            });
+            const chat = localStorage.getItem("chat");
+            if (chat !== null && chat !== "") {
+                reader.pos.mainbox = reader.pos.boxes[+chat];
             }
             else {
                 //If multiple boxes are found, this will select the first, which should be the top-most chat box on the screen.
                 reader.pos.mainbox = reader.pos.boxes[0];
             }
-            showSelectedChat(reader.pos);
+            // console.log(reader);
+            showSelectedChat(reader.pos.mainbox);
             chatboxInterval = setInterval(() => {
                 readChatbox();
             }, 500);
         }
     }, 1000);
     let timestamps = new Set();
-    let actions = 1;
     function readChatbox() {
         const opts = reader.read() || [];
         let chat = "";
@@ -4444,7 +4619,7 @@ function capture() {
             chat += opts[a].text + " ";
         }
         //console.log(chat);
-        const clueComplete = chat.match(/\[\d{2}:\d{2}:\d{2}\] Your (reward was stored in Charos|Charos Clue Carrier has placed)/g);
+        const clueComplete = chat.match(regex);
         // TODO: Auto-stop Alt1 in-app timer if got reward but clue carrier didn't place new clue (last clue finished)
         if (clueComplete != null && clueComplete.length > -1) {
             console.log(clueComplete);
@@ -4456,7 +4631,7 @@ function capture() {
                 else {
                     timestamps.add(timestamp[0]);
                     console.log("SPLIT!");
-                    split(actions);
+                    split();
                     actions++;
                 }
             }
@@ -4479,6 +4654,9 @@ function onInitFs(fs) {
     });
 }
 window.webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024, onInitFs, onError);
+function hexToRgb(hex) {
+    return hex.match(/[A-Za-z0-9]{2}/g).map((v) => parseInt(v, 16));
+}
 //check if we are running inside alt1 by checking if the alt1 global exists
 if (window.alt1) {
     //tell alt1 about the app
@@ -4486,10 +4664,18 @@ if (window.alt1) {
     //also updates app settings if they are changed
     alt1.identifyAppUrl("./appconfig.json");
     if (!alt1.permissionPixel) {
-        timerEle.innerHTML = "<small>Page is not installed as app or capture permission is not enabled</small>";
+        setError("Page is not installed as app or capture permission is not enabled");
     }
     document.addEventListener("readystatechange", () => {
         if (document.readyState === "complete") {
+            const ls = localStorage.getItem("livesplit");
+            if (ls === "" || ls === null) {
+                localStorage.setItem("livesplit", "true");
+            }
+            const color = localStorage.getItem("color");
+            if (color === "" || color === null) {
+                localStorage.setItem("color", "#00ff00");
+            }
             capture();
         }
     }, false);
@@ -4501,22 +4687,22 @@ if (window.alt1) {
 }
 if (document.location.host !== "californ1a.github.io") {
     const th = document.createElement("th");
-    th.innerHTML = "<button class=\"nisbutton test\">Test</button>";
+    th.innerHTML = "<div class=\"nisbutton2 test\">Test</div>";
     document.querySelector(".menu tr").appendChild(th);
 }
-const testBtn = document.querySelector(".nisbutton.test");
+const testBtn = document.querySelector(".test");
 if (testBtn) {
     testBtn.addEventListener("click", test);
 }
-let testAct = 1;
 function test() {
     if (!startTime) {
+        actions = 1;
         startTime = (new Date()).getTime();
         requestAnimationFrame(timer);
         return;
     }
-    split(testAct);
-    testAct++;
+    split();
+    actions++;
 }
 
 })();
